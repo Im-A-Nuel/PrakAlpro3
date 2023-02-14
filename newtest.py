4e1faeaa7d9fa8182548f17d05e36977df006577
Imanuel <118039925+Im-A-Nuel@users.noreply.github.com>
2023-02-14 11:43:18 +0700

    Create newtest.py

---
+++ newtest.py
@@ -0,0 +1,16 @@
+#input
+a=(input('x = '))
+#proses
+# ribuan=a//1000
+# a=a%1000
+# ratusan=a//100
+# a=a%100
+# puluhan=a//10
+# satuan=a%10
+# total=ribuan+ratusan+puluhan+satuan
+total=0
+for digit in a:
+    total=total+int(digit)
+
+#output
+print('Total semua digit : ', total)
